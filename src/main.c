76593019983fdd780f743fe0c1fe2db6a3505482
Nameless-Dev0 <youssef.samir.mls@gmail.com>
2025-09-14 02:22:12 +0300

    Seperated repl from main

--- src/main.c
+++ src/main.c
@@ -1,34 +1,11 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include "lexer.h"
+#include "repl.h"
 
-// main just executes a simple REPL.
+/*
+    main simply executes a REPL pattern.
+*/
 
 int main(void){
-    
-    char* line = NULL;
-    size_t line_buffer_length = 0;
-    ssize_t read_size = 0;
-
-    printf("Welcome to mEval, (Ctrl+D to exit):\n");
-    printf(">> ");
-    fflush(stdout);
-
-    while(1){
-        read_size = getline(&line, &line_buffer_length, stdin);
-        if(read_size == -1)
-            break;
-
-        if(read_size > 0 && line[read_size - 1] == '\n')
-            line[read_size - 1] = '\0';
-            
-        printf("Result: %s\n", line);
-        printf(">> ");
-        fflush(stdout);
-    }
-
-    free(line);
+    repl_loop();
     return 0;
 } 
 
